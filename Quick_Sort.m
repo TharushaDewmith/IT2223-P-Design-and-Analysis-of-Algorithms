4d07503a8cd0f5185f0fc1a8761d67a09734f4d1
TharushaDewmith <tharuwadewmith123@gmail.com>
2025-06-09 00:27:48 +0530

    Add files via upload

---
+++ Quick_Sort.m
@@ -0,0 +1,21 @@
+arr=[5,4,10,1,6,2];
+
+function sortedArr=quickSort(arr)
+if length(arr)<=1
+    sortedArr=arr;
+    return;
+end
+
+pivot=arr(1);
+leftArr=arr(arr<pivot);
+rightArr=arr(arr>pivot);
+
+sortedArr=[quickSort(leftArr),pivot,quickSort(rightArr)];
+
+end
+
+disp('Unsorted array:');
+disp(arr);
+sortedArr=quickSort(arr);
+disp("sorted Array:")
+disp(sortedArr);
